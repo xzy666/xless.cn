ed35f68a3fce9ce340ad9e495487c970f7048e57
xyz666 <504578726@qq.com>
2017-08-30 11:52:33 +0800

    add sth about Article api

--- app/Http/Controllers/api/ArticleController.php
+++ app/Http/Controllers/api/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use App\Repositories\ArticleRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -33,8 +34,65 @@ class ArticleController extends ApiController
      * 返回全部文章信息
      * @return mixed
      */
-    public function articles()
+    public function index()
     {
         return $this->responseWithArray($this->article->page(),new ArticleTransformer);
+    }
+
+    /**
+     * @param ArticleRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(ArticleRequest $request)
+    {
+        $data=[
+            'user_id'=>\Auth::id(),
+            'last_user_id'=>\Auth::id()
+        ];
+        $data['is_draft']    = isset($data['is_draft']);
+        $data['is_original'] = isset($data['is_original']);
+
+        $this->article->store($data);
+
+        $this->article->syncTag(json_decode($request->get('tags')));
+        return $this->noContent();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function edit($id)
+    {
+        return $this->respondWithItem($this->article->getById($id), new ArticleTransformer);
+    }
+
+    /**
+     * @param ArticleRequest $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(ArticleRequest $request, $id)
+    {
+        $data = array_merge($request->all(), [
+            'last_user_id' => \Auth::id()
+        ]);
+
+        $this->article->update($id, $data);
+
+        $this->article->syncTag(json_decode($request->get('tags')));
+
+        return $this->noContent();
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $this->article->destroy($id);
+
+        return $this->noContent();
     }
 }
